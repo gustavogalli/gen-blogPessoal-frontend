71296eb4b5e16a1101e66341ae2ef552d26ee2c0
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-21 19:07:35 -0300

    Criando o método cadastrar() em cadastrar.component.ts

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuarioLogin } from '../model/UsuarioLogin';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -8,23 +10,44 @@ import { UsuarioLogin } from '../model/UsuarioLogin';
 })
 export class CadastrarComponent implements OnInit {
 
-  usuarioLogin: UsuarioLogin = new UsuarioLogin
+  usuario: Usuario = new Usuario
   senhaEntry: string
   tipoEntry: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService, // injeção de dependência
+    private router: Router
+  ) { }
 
   ngOnInit() { // método chamado quando a página iniciar
-    window.scroll(0,0) // vai pro topo da tela, eixo X e Y em 0
+    window.scroll(0, 0) // vai pro topo da tela, eixo X e Y em 0
   }
 
-  confirmarSenha(event: any){
+  confirmarSenha(event: any) { // pega o valor da senha
     this.senhaEntry = event.target.value
   }
 
-  tipoUsuario(event: any){
+  tipoUsuario(event: any) { // pega o valor do tipo de usuário
     this.tipoEntry = event.target.value
   }
 
+  cadastrar() { // não pega nada, por isso não tem parâmetro
 
+    // pega o tipo escolhido e atribui ao tipo do usuario
+    this.usuario.tipo = this.tipoEntry
+
+    // confirma se a senha digitada bate com a senha confirmada
+    if (this.usuario.senha != this.senhaEntry) {
+      alert('A senha digitada não confere!')
+
+    } else {// envia o usuario como objeto para o servidor, mas o SUBSCRIBE transforma em JSON
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {this.usuario = resp})
+      // cadastrar() envia para o servidor
+      // subscribe() transforma o objeto em JSON
+      // arrow function pega a resposta e atribui ao usuario
+
+      this.router.navigate(['/entrar'])
+      alert('Usuário cadastrado com sucesso')
+    }
+  }
 }
