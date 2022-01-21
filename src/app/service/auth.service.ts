26daf190b93ba60d9b9c3dd223b4c207c6e09dc3
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-21 18:12:30 -0300

    Implementando métodos 'entrar' e 'cadastrar' em auth.service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +13,12 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  entrar(){
-
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{ //observable GARANTE o tipo que será passado
+    return this.http.post<UsuarioLogin>("https://blogpessoalgustavogalli.herokuapp.com/usuarios/logar", usuarioLogin) // salva no caminho em questão o parâmetro passado (usuarioLogin)
   }
 
-  cadastrar(){
+  cadastrar(usuario: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>("https://blogpessoalgustavogalli.herokuapp.com/usuarios/logar", usuario)
 
   }
 }
