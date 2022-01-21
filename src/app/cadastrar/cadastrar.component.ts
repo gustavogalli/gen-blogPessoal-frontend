7ea15f5c224b84b07cf669e9072c6a48d807bae2
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-21 18:33:36 -0300

    Criando o método confirmarSenha() em cadastrar.component.ts

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioLogin } from '../model/UsuarioLogin';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
+  usuarioLogin: UsuarioLogin = new UsuarioLogin
+  senhaEntry: string
+  tipoEntry: string
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() { // método chamado quando a página iniciar
+    window.scroll(0,0) // vai pro topo da tela, eixo X e Y em 0
   }
+
+  confirmarSenha(event: any){
+    this.senhaEntry = event.target.value
+  }
+
 
 }
