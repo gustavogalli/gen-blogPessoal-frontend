ff12ccbf1971f2b7beb967a1237f4562521a45ec
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-27 12:47:19 -0300

    Criando variável token na service de Tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient /* acessa os métodos HTTP */) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
 }
