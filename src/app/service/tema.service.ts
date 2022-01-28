7a2ca7dabd47fc3016308a63f199b257c7329d8b
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-28 17:28:24 -0300

    Criando o método getById em tema.service.ts

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,6 +19,10 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoalgustavogalli.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoalgustavogalli.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalgustavogalli.herokuapp.com/tema', tema, this.token)
   }
