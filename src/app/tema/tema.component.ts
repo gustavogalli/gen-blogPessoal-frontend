2cc0d28ff384e8222d0ec8f2119c2a61b83408e7
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-27 22:46:33 -0300

    Inserindo o método findAllTema() dentro do método cadastrar()

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -39,6 +39,7 @@ export class TemaComponent implements OnInit {
     this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
       this.tema = resp
       alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
       this.tema = new Tema()
     })
   }
