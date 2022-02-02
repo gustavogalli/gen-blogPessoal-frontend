2648a301fd2c68fc5592c0dd9f317a0e58a37036
Gustavo Galli <gustavosgalli@gmail.com>
2022-02-02 10:24:46 -0300

    Criando rotas em app-routing.module.ts para componentes postagem-edit e postagem-delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostagemDeleteComponent } from './edit/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaDeleteComponent } from './edit/tema-delete/tema-delete.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
@@ -20,7 +22,9 @@ const routes: Routes = [
   {path:'tema', component:TemaComponent},
 
   {path:'tema-edit/:id', component: TemaEditComponent}, // -> /: <- é como passamos parâmetro no endpoint
-  {path:'tema-delete/:id', component: TemaDeleteComponent}
+  {path:'tema-delete/:id', component: TemaDeleteComponent},
+  {path: 'postagem-edit/:id', component: PostagemEditComponent},
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
 ];
 
 @NgModule({
