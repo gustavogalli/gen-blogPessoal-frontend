efffefd0e8ccd3af4c159a1edcd88d218a4762bc
Gustavo Galli <gustavosgalli@gmail.com>
2022-02-02 11:50:57 -0300

    Inserindo métodos para editar postagem

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Postagem } from 'src/app/model/Postagem';
+import { Tema } from 'src/app/model/Tema';
 import { PostagemService } from 'src/app/service/postagem.service';
+import { TemaService } from 'src/app/service/tema.service';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
@@ -12,11 +14,15 @@ import { environment } from 'src/environments/environment.prod';
 export class PostagemEditComponent implements OnInit {
 
   postagem: Postagem = new Postagem()
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private postagemService: PostagemService
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -26,6 +32,7 @@ export class PostagemEditComponent implements OnInit {
 
     let id = this.route.snapshot.params['id']
     this.findByIdPostagem(id)
+    this.findAllTemas()
   }
 
   findByIdPostagem(id: number){
@@ -34,8 +41,27 @@ export class PostagemEditComponent implements OnInit {
     })
   }
 
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
   atualizar(){
-    
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.postagemService.putPostagem(this.postagem).subscribe((resp: Postagem) =>{
+      this.postagem = resp
+      alert('Postagem atualizada com sucesso!')
+      this.router.navigate(['/inicio'])
+    })
   }
 
 }
