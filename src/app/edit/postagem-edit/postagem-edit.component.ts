04845ac23f4bb16d5b81d1e4c5d3d5a6c7dc839b
Gustavo Galli <gustavosgalli@gmail.com>
2022-02-02 10:43:20 -0300

    Criando métodos em postagem-edit

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from 'src/app/model/Postagem';
+import { PostagemService } from 'src/app/service/postagem.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-postagem-edit',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemEditComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagemService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdPostagem(id)
+  }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) => {
+      this.postagem = resp
+    })
+  }
+
+  atualizar(){
+    
   }
 
 }
