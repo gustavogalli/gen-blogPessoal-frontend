fda461d2ab862f8fccebfbd52b4c1e0695e38391
Gustavo Galli <gustavosgalli@gmail.com>
2022-02-02 21:44:00 -0300

    Alterando de msg para message

--- src/app/service/alertas.service.ts
+++ src/app/service/alertas.service.ts
@@ -11,22 +11,22 @@ export class AlertasService {
     private bsModalService: BsModalService
   ) { }
 
-  private showAlert(msg: string, tipo: string){
+  private showAlert(message: string, tipo: string){
     const bsModalRef: BsModalRef = this.bsModalService.show(AlertasComponent)
     bsModalRef.content.type = tipo
-    bsModalRef.content.message = msg
+    bsModalRef.content.message = message
   }
 
-  showAlertDanger(msg: string){
-    this.showAlert(msg, 'danger')
+  showAlertDanger(message: string){
+    this.showAlert(message, 'danger')
   }
 
-  showAlertSuccess(msg: string){
-    this.showAlert(msg, 'success')
+  showAlertSuccess(message: string){
+    this.showAlert(message, 'success')
   }
 
-  showAlertInfo(msg: string){
-    this.showAlert(msg, 'info')
+  showAlertInfo(message: string){
+    this.showAlert(message, 'info')
   }
 
 }
