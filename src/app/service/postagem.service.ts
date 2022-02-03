89838a0cc3ef9a2132e1b88fa74e6c0d45631323
Gustavo Galli <gustavosgalli@gmail.com>
2022-02-02 23:10:05 -0300

    Criando método getByTituloPostagem na service de Postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -23,6 +23,10 @@ export class PostagemService {
     return this.http.get<Postagem>(`https://blogpessoalgustavogalli.herokuapp.com/postagens/${id}`, this.token)
   }
 
+  getByTituloPostagem(titulo: string): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>(`https://blogpessoalgustavogalli.herokuapp.com/postagens/titulo/${titulo}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogpessoalgustavogalli.herokuapp.com/postagens', postagem, this.token)
   }
